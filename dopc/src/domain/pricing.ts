3016abe84becd3bb6705ce527c6a079ed844d3bd
Simos Tigkas <suomikendam@gmail.com>
2026-01-10 23:31:32 +0200

    inital setup and did the first three calculation functions

---
+++ dopc/src/domain/pricing.ts
@@ -0,0 +1,22 @@
+export type DistanceRange = {
+    min: number;
+    max: number;
+    a: number;
+    b: number;
+}
+
+export type DeliveryPricing = {
+    basePrice: number;
+    distanceRanges: DistanceRange[];
+}
+
+export function calculateSmallOrderSurcharge(cartValue: number, orderMinimumNoSurcharge: number): number {
+    return Math.max(0, orderMinimumNoSurcharge - cartValue);
+}
+
+export function caclulateDeliveryFee(distanceInMeters: number, pricing: DeliveryPricing): number {
+    const range = pricing.distanceRanges.find(r => distanceInMeters >= r.min && (r.max === 0 || distanceInMeters < r.max));
+    if (!range || range.max === 0)
+      throw new Error("Delivery not possible for this distance");
+    return pricing.basePrice + range.a + Math.round(range.b * distanceInMeters/10);
+}
